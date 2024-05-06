5606c1dd84b777ab1f881fa5b8028c041f1413e2
Eric Frankenberger <efrankie@gmail.com>
2024-05-05 23:31:28 -0400

    initial commit with .json from sheet, maps, and screenshots

--- python/config.py
+++ python/config.py
@@ -1,10 +1,13 @@
-#SHEET_ID = '1om84pRBMUvmVxD6ckd4u9imY9qGa1vMNPZ79WiKz9ig'
-SHEET_ID = '13sUolfNnFm6_Y6Z2O9DiF5_FUe7b6XCT5Q84l87b5hQ' # backup sheet ID
+import os
+
+#SHEET_ID = '1om84pRBMUvmVxD6ckd4u9imY9qGa1vMNPZ79WiKz9ig' # prod sheet ID
+SHEET_ID = '13sUolfNnFm6_Y6Z2O9DiF5_FUe7b6XCT5Q84l87b5hQ' # testgit s sheet ID
 RANGE_NAME = "'Surf maps'" # name of the sheet, has to be in ''s because of space in name
 
 MAPS_FOLDER_ID = '1BwsG6pIsRFiCGGG7ppiLSJ0NbLv7_Xv4'
 SCREENSHOTS_FOLDER_ID = '1pHuvPJZn0UPAd4mn8y5coXBDRV3mKTFu'
 
-SHEET_DATA_FILE_NAME = 'sheet_data.json'
-SCREENSHOTS_DATA_FILE_NAME = 'screenshots_data.json'
-MAPS_DATA_FILE_NAME = 'maps_data.json'
+DATA_FOLDER = 'data'
+SHEET_DATA_FILE_NAME = os.path.join(DATA_FOLDER, 'sheet_data.json')
+SCREENSHOTS_DATA_FILE_NAME = os.path.join(DATA_FOLDER, 'screenshots_data.json')
+MAPS_DATA_FILE_NAME =  os.path.join(DATA_FOLDER, 'maps_data.json')
--- python/get_drive_data.py
+++ python/get_drive_data.py
@@ -1,6 +1,9 @@
 import config
 import gtoken
+import json
+
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 SCREENSHOTS_FOLDER_ID = config.SCREENSHOTS_FOLDER_ID
 SCREENSHOTS_DATA_FILE_NAME = config.SCREENSHOTS_DATA_FILE_NAME
@@ -9,9 +12,7 @@ MAPS_FOLDER_ID = config.MAPS_FOLDER_ID
 MAPS_DATA_FILE_NAME = config.MAPS_DATA_FILE_NAME
 
 # list of folders with their associated output file name
-FOLDER_AND_FILE_NAME_PAIRS = [(SCREENSHOTS_FOLDER_ID, SCREENSHOTS_DATA_FILE_NAME), (MAPS_FOLDER_ID), (MAPS_DATA_FILE_NAME)]
-
-print(type(FOLDER_AND_FILE_NAME_PAIRS))
+FOLDER_AND_FILE_NAME_PAIRS = [(SCREENSHOTS_FOLDER_ID, SCREENSHOTS_DATA_FILE_NAME), (MAPS_FOLDER_ID, MAPS_DATA_FILE_NAME)]
 
 def get_drive_items(folder_id, output_file_name):
     creds = gtoken.get()
@@ -22,21 +23,32 @@ def get_drive_items(folder_id, output_file_name):
     screenshot_filenames = []
 
     while True:
-        results = drive_service.files().list(q=screenshots_query, fields="nextPageToken, files(id, name, mimeType)", pageSize=page_size, pageToken=page_token).execute()
+        try:
+            results = drive_service.files().list(q=screenshots_query, fields="nextPageToken, files(id, name, mimeType)", pageSize=page_size, pageToken=page_token).execute()
 
-        items = results.get('files', [])
-        if not items:
-            print('No files or folders found.')
-        else:
-            for item in items:
-                if item.get('mimeType') != 'application/vnd.google-apps.folder':
-                    screenshot_filenames.append(item)  # Only add files to the drive_items list
-                else:
-                    print("non-file found ", item)
+            items = results.get('files', [])
+            if not items:
+                print('No files or folders found.')
+            else:
+                for item in items:
+                    if item.get('mimeType') != 'application/vnd.google-apps.folder':
+                        screenshot_filenames.append(item)  # Only add files to the drive_items list
+                    else:
+                        print("non-file found ", item)
 
-        # Check if there are more pages of results
-        page_token = results.get('nextPageToken')
-        if not page_token:
-            break
+            # Check if there are more pages of results
+            page_token = results.get('nextPageToken')
+            if not page_token:
+                break
+
+        except HttpError as err:
+            print(err)
     
-    return screenshot_filenames
+    with open(output_file_name, 'w') as json_file:
+        json.dump(items, json_file)
+    
+    print(f'Data saved to {output_file_name}')
+
+if __name__ == "__main__":
+    for folder_id, output_file_name in FOLDER_AND_FILE_NAME_PAIRS:
+        get_drive_items(folder_id, output_file_name)
